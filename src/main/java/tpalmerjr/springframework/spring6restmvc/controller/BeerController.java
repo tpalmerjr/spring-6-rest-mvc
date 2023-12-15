7c09a09b27569a8d172827c5a943b8e47fc5e130
Terry Palmer <tpalmerjr@gmail.com>
2023-12-15 14:30:51 -0600

    9-http-post

--- src/main/java/tpalmerjr/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/tpalmerjr/springframework/spring6restmvc/controller/BeerController.java
@@ -2,10 +2,9 @@ package tpalmerjr.springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tpalmerjr.springframework.spring6restmvc.model.Beer;
 import tpalmerjr.springframework.spring6restmvc.services.BeerService;
 
@@ -18,6 +17,15 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+//    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
